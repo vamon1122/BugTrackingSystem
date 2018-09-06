f2f54d9f8bbbccd83ec43dd8bad484e02024e449
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2018-09-06 12:18:03 +0100

    Database working again

---
+++ Log/Log/DefaultLog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenLog
+{
+    public static class AppLog
+    {
+        private static Log DefaultLog = new Log("log.txt", AppDomain.CurrentDomain.BaseDirectory);
+        
+        //Work
+        //private static Log DefaultLog = new Log(@"C:\Users\Ben\Desktop\BTS Class Lib + Con Test (Friday 5PM)\log.txt");
+
+        //Home
+        //private static Log DefaultLog = new Log(@"M:\Users\benba\My Documents\Production\Code Projects\C#\Bug Tracking System\BTS Class Lib + Con Test\log.txt");
+
+        public static void Break()
+        {
+            DefaultLog.Break();
+        }
+
+        public static void Input(string value)
+        {
+            DefaultLog.Input(value);
+        }
+
+        public static void Output(string value)
+        {
+            DefaultLog.Output(value);
+        }
+
+        public static void Func(string value)
+        {
+            DefaultLog.Func(value);
+        }
+
+        public static void Debug(string value)
+        {
+            DefaultLog.Debug(value);
+        }
+
+        public static void Info(string value)
+        {
+            DefaultLog.Info(value);
+        }
+
+        public static void Error(string value)
+        {
+            DefaultLog.Error(value);
+        }
+    }
+}
---
+++ Log/Log/LogClass.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BenLog
+{
+    public class Log
+    {
+        private string _fileName;
+        private string _filePath;
+
+        public Log(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public Log(string fileName, string fileDirectory)
+        {
+            _fileName = fileName;
+            _filePath = fileDirectory;
+        }
+
+        private void WriteLog(string prefix, string value)
+        {
+            if (_filePath != null)
+            {
+                if (!System.IO.Directory.Exists(_filePath))
+                {
+                    System.IO.Directory.CreateDirectory(_filePath);
+                }
+
+                if (_filePath.Substring(_filePath.Length - 1, 1) != @"\")
+                {
+                    _filePath += @"\";
+                }
+            }
+
+            if (File.Exists(_filePath + _fileName))
+            {
+                //Console.WriteLine("Log exists!");
+                using (StreamWriter log = File.AppendText(_filePath + _fileName))
+                {
+                    log.WriteLine("<{0}> {1} - {2}", prefix, GetDateTime(), value);
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (FileStream fileStream = new FileStream(_filePath + _fileName, FileMode.OpenOrCreate))
+                    {
+
+                        using (StreamWriter log = new StreamWriter(fileStream))
+                        {
+                            log.WriteLine("<info> {0} - Log \"{1}\" did not exist. Log has been created", GetDateTime(), _fileName);
+                            log.WriteLine("<{0}> {1} - {2}", prefix, GetDateTime(), value);
+                        }
+                    }
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.WriteLine("Error while creating log. Directory does not exist!");
+                    Console.ReadLine();
+                }
+                
+            }
+        }
+
+        private void WriteLog()
+        {
+            if (_filePath != null)
+            {
+                if (!System.IO.Directory.Exists(_filePath))
+                {
+                    System.IO.Directory.CreateDirectory(_filePath);
+                }
+
+                if (_filePath.Substring(_filePath.Length - 1, 1) != @"\")
+                {
+                    _filePath += @"\";
+                }
+            }
+
+            if (File.Exists(_filePath + _fileName))
+            {
+                //Console.WriteLine("Log exists!");
+                using (StreamWriter log = File.AppendText(_filePath + _fileName))
+                {
+                    log.WriteLine("");
+                }
+            }
+            else
+            {
+                using (FileStream fileStream = new FileStream(_filePath + _fileName, FileMode.OpenOrCreate))
+                {
+
+                    using (StreamWriter log = new StreamWriter(fileStream))
+                    {
+                        log.WriteLine("<info> {0} - Log \"{1}\" did not exist. Log has been created", GetDateTime(), _fileName);
+                        log.WriteLine("");
+                    }
+                }
+            }
+        }
+
+        private static string GetDateTime()
+        {
+            return System.DateTime.Now.ToString();
+        }
+
+        public void Break()
+        {
+            WriteLog();
+        }
+
+        public void Input(string value)
+        {
+            WriteLog("input", value);
+        }
+
+        public void Output(string value)
+        {
+            WriteLog("output", value);
+        }
+
+        public void Func(string value)
+        {
+            WriteLog("function", value);
+        }
+
+        public void Debug(string value)
+        {
+            WriteLog("debug", value);
+        }
+
+        public void Info(string value)
+        {
+            WriteLog("info", value);
+        }
+
+        public void Error(string value)
+        {
+            WriteLog("error", value);
+        }
+    }
+
+    
+}
